bedf21aefea50ea3471a7baea11449ad2ba75f8c
Lucas Postai Hernandes <69318257+LucasPHernandes@users.noreply.github.com>
2022-02-04 21:28:06 -0300

    Commit

---
+++ app.py
@@ -0,0 +1,100 @@
+import bcrypt
+from flask import Flask, flash, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm 
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+
+
+app = Flask('__name__')
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'chavesecreta'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Definindo O Usuário
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+# Criando Formulário de Registro
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField("Registrar")
+
+    def validate_username(self, username):
+        existing_user_name = User.query.filter_by(username=username.data).first()
+
+        if existing_user_name:
+            raise ValidationError('O nome de usuário já existe, Por favor escolha outro.')
+
+# Criando Formulário de Login
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField("Login")
+
+
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    return render_template('home.html')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                user_id = current_user.id
+                return redirect(url_for('dashboard', user_id=user_id))
+    return render_template('login.html', form=form)
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    
+    return render_template('register.html', form=form)
+
+@app.route('/logout', methods=['POST', 'GET'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/dashboard/<user_id>', methods=['POST', 'GET'])
+@login_required
+def dashboard(user_id):
+    user_id = user_id
+    return render_template('dashboard.html', user_id=user_id)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+# Criar um programa para criar FlashCards para ajudar nos estudos
+# Ao clicar em um FlahsCard, o mesmo abrirá uma página com mais detalhes do mesmo [vai ser difícil fazer isso...]
